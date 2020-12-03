8810abd81ba9feb1c78bfe130ac72d7b62724e26
Roisin Mcphillips <57397919+D00219161@users.noreply.github.com>
2020-12-03 23:25:52 +0000

    Updated the PubNub connection for the Pi's
    Styled the index.html page, got some database working,
    got the connection to pubnub ready for the pi's, and
    added the logo

--- __init__.py
+++ __init__.py
@@ -1,16 +1,79 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, url_for, session, flash
+import json, string, random, hashlib
 
+# Mongo Database import connection
+import pymongo
+from pymongo import MongoClient, dnspython
+
+from functools import wraps
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+
+# Mongo DB Connection
+cluster = MongoClient("mongodb+srv://Roisin:DFM5CauDv8K9tXpY@cluster0.b528o.mongodb.net/home_safe?retryWrites=true&w=majority")
+# Mongo Database Config
+db = cluster["home_safe"]
+collection = db["electricity_usage"], db["temp_usage"], db["users"]
+
+db = cluster(app)
+
+from . import homesafeDB, PB
 
 alive = 0
 data = {}
 
+# Grant read and write access to the authkey "SD3b-Raspberry-Pi" - More than One Pi
+PB.grant_access("Homesafe-Matthew-Raspberry-Pi", True, True)  # Matthew's Pi Connection to read & write
+PB.grant_access("Homesafe-Finbar-Raspberry-Pi", True, True)  # Finbar's Pi Connection to read & write
 
-@app.route("/")
+
+@app.route("/login")
 def index():
     return render_template("index.html")
+
+
+def LoginRequired(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'logged_in' in session:
+            if session['logged_in']:
+                return f(*args, **kwargs)
+        flash("Please login first")
+        return redirect(url_for('login'))
+
+    return wrapper
+
+
+@app.route("/main")
+@LoginRequired
+def main():
+    flash(session["user"])
+    homesafeDB.add_user_and_login(session['user'], int(session['user_id']))
+    homesafeDB.view_all()
+    return render_template("index.html", user_id=session['user_id'], online_users=homesafeDB.get_all_logged_in_users())
+
+
+def clear_session():
+    session['logged_in'] = None
+    session['user'] = None
+    session['user_id'] = None
+
+
+@app.route("/")
+def login():
+    clear_session()
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    homesafeDB.user_logout(session['user_id'])
+    homesafeDB.view_all()
+    clear_session()
+    flash("You just logged out")
+    return redirect(url_for("login"))
 
 
 @app.route("/keep_alive", methods=["GET"])
@@ -22,6 +85,56 @@ def keep_alive():
     parsed_json = json.dumps(data)
     print(str(parsed_json))
     return str(parsed_json)
+
+
+def str_to_bool(s):
+    if 'true' in str(s):
+        return True
+    elif 'false' in str(s):
+        return False
+    else:
+        raise ValueError
+
+
+@app.route("/grant-<user_id>-<read>-<write>", methods=["POST", "GET"])
+def grant_access(user_id, read, write):
+    if int(session['user_id']) == 1884560171685373:
+        print("Granting " + user_id + " read: " + read + " write: " + write + " permission")
+        # store user read/write permissions into the database
+        homesafeDB.add_user_permission(user_id, read, write)
+        auth_key = homesafeDB.get_authkey(user_id)
+        # grant PubNub read/write access
+        PB.grant_access(auth_key, str_to_bool(read), str_to_bool(write))
+    else:
+        print("Who are you?")
+        return json.dumps({"access":"denied"})
+    return json.dumps({"access":"granted"})
+
+
+# Salting
+def salt(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+# Creating auth key for user
+def create_auth_key():
+    s = salt(10)
+    hashing = hashlib.sha256((str(session['user_id']) + s).encode('utf-8'))
+    return hashing.hexdigest()
+
+
+@app.route("/get_auth_Key", methods=["POST", "GET"])
+def get_auth_key():
+    print("Creating authkey for " + session['user'])
+    auth_key = create_auth_key()
+    homesafeDB.add_authkey(int(session['user_id']), auth_key)
+    (read, write) = homesafeDB.get_user_access(int(session['user_id']))
+    # Use PubNub to grant these privileges to this user
+    PB.grant_access(auth_key, read, write)
+    auth_response = {"authKey":auth_key, "cipherKey":PB.cipherKey}
+    json_response = json.dumps(auth_response)
+    return str(json_response)
+    return str("authkey")
 
 
 if __name__ == "__main__":
---
+++ homesafeDB.py
@@ -0,0 +1,176 @@
+# Mongo Database import connection
+import pymongo
+from pymongo import MongoClient, dnspython
+
+# Importing the db
+from .__init__ import db
+
+# Writing the Database Connections for home_safe
+# First collection is users
+
+
+class HomeSafe(db.collectio):
+    __collection__ = db["users"]
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.string(4096))
+    surname = db.Column(db.string(4096))
+    password = db.Column(db.string(4096))
+    phoneNumber = db.Column(db.Integer)
+    emailAddress = db.Column(db.string(4096))
+    address = db.Column(db.string(4096))
+    postCode = db.Column(db.string(4096))
+    dateOfBirth = db.Column(db.string(4096))
+    gender = db.Column(db.string(4096))
+    # User access - id, authkey, login & read
+    user_id = db.Column(db.Integer)
+    authkey = db.Column(db.String(4096))
+    login = db.Column(db.Integer)
+    read_access = db.Column(db.Integer)
+
+    def __init__(self, firstname, surname, password, phoneNumber, emailAddress, address, postCode,
+                 dateOfBirth, gender, user_id, authkey, login, read_access):
+        self.firstname = firstname
+        self.surname = surname
+        self.password = password
+        self.phoneNumber = phoneNumber
+        self.emailAddress = emailAddress
+        self.address = address
+        self.postCode = postCode
+        self.dateOfBirth = dateOfBirth
+        self.gender = gender
+        self.user_id = user_id
+        self.authkey = authkey
+        self.login = login
+        self.read_access = read_access
+
+
+def delete_all():
+    try:
+        db.session.query(HomeSafe).delete()
+        db.session.commit()
+        print("Delete all finished")
+    except Exception as e:
+        print("Failed " + str(e))
+        db.session.rollback()
+
+
+def get_user_row_if_exists(user_id):
+    get_user_row = HomeSafe.query.filter_by(user_id=user_id).first()
+    if (get_user_row != None):
+        return get_user_row
+    else:
+        print("User does not exist")
+        return False
+
+
+def add_user_and_login(firstname, user_id):
+    row = get_user_row_if_exists(user_id)
+    if row != False:
+        row.login = 1
+        db.session.commit()
+    else:
+        print("Adding user " + firstname)
+        new_user = HomeSafe(firstname, firstname, surname, password, phoneNumber, emailAddress, address, postCode,
+                            dateOfBirth, gender, user_id, None, 1, 0)
+        db.session.add(new_user)
+        db.session.commit()
+    print("User " + firstname + " login added")
+
+
+def bool_to_int(v):
+    if 'true' in str(v):
+        return 1
+    elif 'false' in str(v):
+        return 0
+    else:
+        raise ValueError
+
+
+def add_user_permission(user_id, read):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        row.read_access = bool_to_int(read)
+        db.session.commit()
+        print("User permission added")
+
+
+def user_logout(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        row.login = 0
+        db.session.commit()
+        print("User " + row.firstname + "logout updated")
+
+
+def add_authkey(user_id, authkey):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        row.authkey = authkey
+        db.session.commit()
+        print("User " + row.authkey + "authkey added")
+
+
+def get_authkey(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        return row.authkey
+    else:
+        print("User with id " + user_id + " doesn't exist")
+
+
+def get_user_access(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row:
+        get_user_row = HomeSafe.query.filter_by(user_id=user_id).first()
+        read = get_user_row.read_access
+        if read == 1:
+            read = True
+        else:
+            read = False
+    return read
+
+
+def view_all():
+    row = HomeSafe.query.all()
+    for n in range(0, len(row)):
+        print(str(row[n].id) + " | " +
+              row[n].firstname + " | " +
+              row[n].surname + " | " +
+              row[n].password + " | " +
+              str(row[n].phoneNumber) + " | " +
+              row[n].emailAddress + " | " +
+              row[n].address + " | " +
+              row[n].postCode + " | " +
+              row[n].dateOfBirth + " | " +
+              row[n].gender + " | " +
+              str(row[n].user_id) + " | " +
+              str(row[n].authkey) + " | " +
+              str(row[n].login))
+
+
+def get_all_logged_in_users():
+    row = HomeSafe.query.filter_by(login=1).all()
+    online_user_record = {"user_record": []}
+    print("LoggedIn Users:")
+    for n in range(0, len(row)):
+        if row[n].read_access:
+            read = "checked"
+        else:
+            read = "unchecked"
+        online_user_record["user_record"].append([row[n].firstname, row[n].surname, row[n].password,row[n].phoneNumbe,
+                                                  row[n].emailAddress, row[n].address, row[n].postCode,
+                                                  row[n].dateOfBirth, row[n].gender, row[n].user_id, read])
+        print(str(row[n].id) + " | " +
+              row[n].firstname + " | " +
+              row[n].surname + " | " +
+              row[n].password + " | " +
+              str(row[n].phoneNumber) + " | " +
+              row[n].emailAddress + " | " +
+              row[n].address + " | " +
+              row[n].postCode + " | " +
+              row[n].dateOfBirth + " | " +
+              row[n].gender + " | " +
+              str(row[n].user_id) + " | " +
+              str(row[n].authkey) + " | " +
+              str(row[n].login))
+    return online_user_record
